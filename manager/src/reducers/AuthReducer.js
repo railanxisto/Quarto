aba093a882b6aff20cde3a48b17c42120c2b15eb
railanxisto <railan.xisto@gmail.com>
2017-02-19 01:01:52 -0300

    Atualizando reducer

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -21,7 +21,7 @@ export default (state = INITIAL_STATE, action) => {
 		case PASSWORD_CHANGED:
 			return { ...state, password: action.payload };
 		case LOGIN_USER_SUCESS:
-			return { ...state, user: action.payload, error: '', loading: false };
+			return { ...state, ...INITIAL_STATE, user: action.payload, };
 		case LOGIN_USER_FAIL:
 			return { ...state, error: 'Authentication Failed!', loading: false };
 		case LOGIN_USER:
