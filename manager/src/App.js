386ecc1bb596ed047ae06e0d2defa31ceed76801
railanxisto <railan.xisto@gmail.com>
2017-02-18 23:45:37 -0300

    Tela de Login funcionando com firebase

---
+++ manager/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'; 
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+	componentWillMount() {
+		const config = { 
+			apiKey: 'AIzaSyAbTV53-9Ob1HP1sxP8Gio3mFqHVWY1z-0',
+			authDomain: 'manager-3b581.firebaseapp.com',
+			databaseURL: 'https://manager-3b581.firebaseio.com',
+			storageBucket: 'manager-3b581.appspot.com',
+			messagingSenderId: '309997616915' };
+		firebase.initializeApp(config);
+	}
+
+	render() {
+		return (
+			<Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+				<LoginForm />
+			</Provider>
+		);
+	}
+}
+
+export default App;
---
+++ manager/src/actions/index.js
@@ -0,0 +1,54 @@
+import firebase from 'firebase'; 
+import { 
+	EMAIL_CHANGED, 
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCESS,
+	LOGIN_USER_FAIL,
+	LOGIN_USER
+} from './types';
+
+export const emailChanged = (text) => {
+	return {
+		type: EMAIL_CHANGED,
+		payload: text
+	};
+};
+
+export const passwordChanged = (text) => {
+	return {
+		type: PASSWORD_CHANGED,
+		payload: text
+	};
+};
+
+export const loginUser = ({ email, password }) => {
+	return (dispatch) => {
+		dispatch({ type: LOGIN_USER });
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(user => loginUserSucess(dispatch, user))
+			.catch((error) => {
+				console.log(error);
+				firebase.auth().createUserWithEmailAndPassword(email, password) 
+					.then(user => loginUserSucess(dispatch, user))
+					.catch((error2) => {
+						loginUserFail(dispatch);
+						console.log(error2);
+					});
+			});
+	};  
+};
+
+const loginUserSucess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCESS,
+		payload: user
+	});
+};
+
+const loginUserFail = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_FAIL,
+		payload: user
+	});
+};
